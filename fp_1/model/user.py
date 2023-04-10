15b5469bf334e6139b069ee105552b54c7f21917
aarya6780 <123915019+aarya6780@users.noreply.github.com>
2023-04-10 02:53:56 -0400

    Add files via upload

--- fp_1/model/user.py
+++ fp_1/model/user.py
@@ -1,25 +1,25 @@
-from app2 import db
-from model.login import LoginVO
-from model.survey import surveyTable
+from app import db
+from model.login import loginTable
 
-class UserVO(db.Model):
+
+class Register(db.Model):
     _tablename_ = 'user_table'
     user_id = db.Column('user_id', db.Integer, primary_key=True, autoincrement=True)
-    user_firstname = db.Column('user_firstname', db.String(100), nullable=False)
-    user_lastname = db.Column('user_lastname', db.String(100), nullable=False)
-    user_email_id = db.Column('user_emailid', db.String(100), nullable=False)
-    user_contact = db.Column('user_contact', db.Numeric, nullable=False)
-    user_login_id = db.Column('user_login_id', db.Integer, db.ForeignKey(LoginVO.login_id))
+    first_name = db.Column('user_firstname', db.String(100), nullable=False)
+    last_name = db.Column('user_lastname', db.String(100), nullable=False)
+    email_id = db.Column('user_emailid', db.String(100), nullable=False)
+    contact = db.Column('user_contact', db.Numeric, nullable=False)
+    user_logged_id = db.Column('user_login_id', db.Integer, db.ForeignKey(loginTable.logged_id))
     # user_survey_id = db.Column('user_survey_id', db.Integer, db.ForeignKey(surveyTable.survey_id), nullable=True)
 
     def as_dict(self):
         return {
             'user_id': self.user_id,
-            'user_firstname': self.user_firstname,
-            'user_lastname': self.user_lastname,
-            'user_email_id' : self.user_email_id,
-            'user_contact': self.user_contact,
-            'user_login_id': self.user_login_id,
+            'user_firstname': self.first_name,
+            'user_lastname': self.last_name,
+            'user_email_id' : self.email_id,
+            'user_contact': self.contact,
+            'user_login_id': self.user_logged_id,
             # 'user_survey_id': self.user_survey_id,
         }
 db.create_all()
